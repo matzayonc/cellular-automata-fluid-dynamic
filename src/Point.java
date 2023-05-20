c842e42fc6058fda64d1d3d4111a773bc1ce6478
Mateusz Zając <matzayonc@gmail.com>
2023-05-20 12:54:16 +0200

    outs of a cell

--- src/Point.java
+++ src/Point.java
@@ -6,8 +6,10 @@ public class Point {
 	public int type;
 	public float color;
 	public int velocity;
-
 	public int vel_direction;
+
+	private boolean[] outs = { false, false, false, false, false, false };
+	private boolean staticParticle = false;
 
 	public Point(boolean isOnEdge) {
 		guard = isOnEdge;
@@ -30,8 +32,9 @@ public class Point {
 	public void clicked() {
 		type = 1;
 		color = 1;
-		velocity=1;
+		velocity = 1;
 		vel_direction = 2;
+
 		for (Point n : neighbors) {
 			n.type = 1;
 			n.color = Math.max(0.7f, n.color);
@@ -40,66 +43,71 @@ public class Point {
 
 	public void clear() {
 		type = 0;
-		// TODO: clear velocity and pressure
+		staticParticle = false;
+		for (int i = 0; i < neighbors.length; ++i)
+			outs[i] = false;
 	}
 
-	public void updateVelocity() {
-		// TODO: velocity update
-	}
-
-	public void updatePresure() {
-		// TODO: pressure update
-	}
 	public void collision2() {
-		Point a = null, b=null;
-		boolean collided=false;
+		Point a = null, b = null;
+		boolean collided = false;
 		if (neighbors[0].vel_direction + neighbors[3].vel_direction == 0) {
-			a=neighbors[0];
-			b=neighbors[3];
+			a = neighbors[0];
+			b = neighbors[3];
 			collided = true;
 
 		}
 		if (neighbors[1].vel_direction + neighbors[4].vel_direction == 0) {
-			a=neighbors[1];
-			b=neighbors[4];
+			a = neighbors[1];
+			b = neighbors[4];
 			collided = true;
 		}
 		if (neighbors[2].vel_direction + neighbors[5].vel_direction == 0) {
-			a=neighbors[2];
-			b=neighbors[5];
+			a = neighbors[2];
+			b = neighbors[5];
 			collided = true;
 		}
-		if(collided){
+		if (collided) {
 			int temp = a.vel_direction;
 			a.vel_direction = b.vel_direction;
 			b.vel_direction = temp;
 		}
 	}
+
 	public void collision3() {
-		Point a = null, b=null, c=null;
-		boolean collided=false;
-		int sum1 = neighbors[0].vel_direction + neighbors[2].vel_direction +neighbors[4].vel_direction;
+		Point a = null, b = null, c = null;
+		boolean collided = false;
+		int sum1 = neighbors[0].vel_direction + neighbors[2].vel_direction + neighbors[4].vel_direction;
 		int sum2 = neighbors[1].vel_direction + neighbors[3].vel_direction + neighbors[5].vel_direction;
 		if (sum1 == 2 || sum1 == -2) {
-			a=neighbors[0];
-			b=neighbors[2];
-			c=neighbors[4];
+			a = neighbors[0];
+			b = neighbors[2];
+			c = neighbors[4];
 			collided = true;
 
 		}
 		if (sum2 == 2 || sum2 == -2) {
-			a=neighbors[1];
-			b=neighbors[3];
-			c=neighbors[5];
+			a = neighbors[1];
+			b = neighbors[3];
+			c = neighbors[5];
 			collided = true;
 		}
 
-		if(collided){
-			a.vel_direction = a.vel_direction*(-1);
-			b.vel_direction = b.vel_direction*(-1);
-			c.vel_direction = c.vel_direction*(-1);
+		if (collided) {
+			a.vel_direction = a.vel_direction * (-1);
+			b.vel_direction = b.vel_direction * (-1);
+			c.vel_direction = c.vel_direction * (-1);
 		}
 	}
+
+	public boolean out(int index) {
+		return outs[index];
+	}
+
+	private boolean in(int index) {
+		return neighbors[index].out((index + 3) % 6);
+	}
+
 	public float getColor() {
 		return color;
 	}
